8d198faf82317ebed2f2b1c4334cbb4036bcd2ad
Thomas-Horvath <thomas.horvath0416@gmail.com>
2024-08-20 11:24:14 +0200

    nanvabritem

--- src/components/HeaderSection/Navbar/Navbar.jsx
+++ src/components/HeaderSection/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React , { useState} from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
@@ -7,37 +7,44 @@ import { IoCloseSharp } from "react-icons/io5";
 import { MdOutlineKeyboardArrowDown } from "react-icons/md";
 
 const Navbar = ({ isOpen, setIsOpen }) => {
+  const [isSublistOpen, setIsSublistOpen] = useState(false);
+
+
+  // const toggleSublist = () => {
+  //   setIsSublistOpen(!isSublistOpen);
+  // };
+
   return (
     <div className={`navbar ${isOpen ? 'open' : ''}`}>
 
       <button className='btn close-btn' onClick={() => setIsOpen(false)}><IoCloseSharp /></button>
       <ul className='navbar-menu w1400'>
-        <li className='navbar-menu-item' onClick={() => setIsOpen(false)}>Elektromos gitárok<MdOutlineKeyboardArrowDown /> 
-          <ul className='sublist'>
-            <Link to="/products"><li>Jobb kezes</li></Link>
-            <Link to="/products"><li>Bal kezes</li></Link>
-            <Link to="/products"><li>Héthúros</li></Link>
+        <li className='navbar-menu-item'>Elektromos gitárok<MdOutlineKeyboardArrowDown /> 
+          <ul className={`sublist ${isSublistOpen ? 'openList' : ''}`}>
+            <Link to="/products" onClick={() => setIsOpen(false)} ><li>Jobb kezes</li></Link>
+            <Link to="/products" onClick={() => setIsOpen(false)} ><li>Bal kezes</li></Link>
+            <Link to="/products" onClick={() => setIsOpen(false)} ><li>Héthúros</li></Link>
           </ul>
         </li>
-        <li className='navbar-menu-item' onClick={() => setIsOpen(false)}>Gitár erősítők<MdOutlineKeyboardArrowDown />
-          <ul className='sublist'>
-            <Link to="/products"><li>Erősítő fej</li></Link>
-            <Link to="/products"><li>Láda</li></Link>
-            <Link to="/products"><li>Kombó</li></Link>
+        <li className='navbar-menu-item'>Gitár erősítők<MdOutlineKeyboardArrowDown />
+          <ul className={`sublist ${isSublistOpen ? 'openList' : ''}`}>
+            <Link to="/products" onClick={() => setIsOpen(false)}><li>Erősítő fej</li></Link>
+            <Link to="/products" onClick={() => setIsOpen(false)}><li>Láda</li></Link>
+            <Link to="/products" onClick={() => setIsOpen(false)}><li>Kombó</li></Link>
           </ul></li>
-        <li className='navbar-menu-item' onClick={() => setIsOpen(false)}>Effektek<MdOutlineKeyboardArrowDown />
-          <ul className='sublist'>
-            <Link to="/products"><li>Pedál</li></Link>
-            <Link to="/products"><li>Multieffekt</li></Link>
+        <li className='navbar-menu-item' >Effektek<MdOutlineKeyboardArrowDown />
+          <ul className={`sublist ${isSublistOpen ? 'openList' : ''}`}>
+            <Link to="/products" onClick={() => setIsOpen(false)}><li>Pedál</li></Link>
+            <Link to="/products" onClick={() => setIsOpen(false)}><li>Multieffekt</li></Link>
             {/* <Link to="/products"><li>Héthúros</li></Link> */}
           </ul></li>
-        <li className='navbar-menu-item' onClick={() => setIsOpen(false)}>Kiegészítők<MdOutlineKeyboardArrowDown />
-          <ul className='sublist'>
-            <Link to="/products"><li>Húr</li></Link>
-            <Link to="/products"><li>Kemény tok</li></Link>
-            <Link to="/products"><li>Puhatok</li></Link>
-            <Link to="/products"><li>Heveder</li></Link>
-            <Link to="/products"><li>Pengető</li></Link>
+        <li className='navbar-menu-item'>Kiegészítők<MdOutlineKeyboardArrowDown />
+          <ul className={`sublist ${isSublistOpen ? 'openList' : ''}`}>
+            <Link to="/products" onClick={() => setIsOpen(false)}><li>Húr</li></Link>
+            <Link to="/products" onClick={() => setIsOpen(false)}><li>Kemény tok</li></Link>
+            <Link to="/products" onClick={() => setIsOpen(false)}><li>Puhatok</li></Link>
+            <Link to="/products" onClick={() => setIsOpen(false)}><li>Heveder</li></Link>
+            <Link to="/products" onClick={() => setIsOpen(false)}><li>Pengető</li></Link>
           </ul></li>
         <li><Link to="/" className='navbar-menu-item' onClick={() => setIsOpen(false)}>Akciók <FaGripfire className='icon' /></Link></li>
       </ul>
---
+++ src/components/HeaderSection/Navbar/NavbarItem/NavbarItem.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './NavbarItem.css'
+
+const NavbarItem = () => {
+  return (
+    <div>NavbarItem</div>
+  )
+}
+
+export default NavbarItem
