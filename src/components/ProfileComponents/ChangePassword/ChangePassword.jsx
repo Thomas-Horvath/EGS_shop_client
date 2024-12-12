954064aae134305a1c187af1ab8bc62a183ca7bd
Thomas-Horvath <thomas.horvath0416@gmail.com>
2024-12-12 16:56:01 +0100

    update change password function

--- src/components/ProfileComponents/ChangePassword/ChangePassword.jsx
+++ src/components/ProfileComponents/ChangePassword/ChangePassword.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { IoEye, IoEyeOffSharp } from "react-icons/io5";
 import './ChangePassword.css';
 
@@ -32,30 +32,35 @@ const ChangePassword = ({ username, email, handleBackClick }) => {
             const loginData = await loginResponse.json();
 
             if (loginResponse.ok && loginData.token) {
-                // Ha a bejelentkezés sikeres, elküldjük az új jelszót
-                const updateResponse = await fetch(`${process.env.REACT_APP_API_URL}/api/profileupdate`, {
-                    method: "PUT",
-                    headers: {
-                        "Content-Type": "application/json; charset=UTF-8",
-                        "Authorization": `Bearer ${loginData.token}`
-                    },
-                    mode: "cors",
-                    body: JSON.stringify({
-                        "UserName": username,
-                        "Password": newPassword,
-                        "EmailAddress": email,
-                    })
-                });
-
-                // const updateData = await updateResponse.json();
-
-                if (updateResponse.ok) {
-                    setSuccessMessage('A jelszó sikeresen megváltoztatva!');
-                    setErrorMessage('');
-                    setTimeout(() => navigate(-1), 2000);
-                } else {
-                    setErrorMessage('Hiba történt a jelszó megváltoztatása során.');
+                // Ha a bejelentkezés sikeres, ellenőrizzük, hogy az új jelszó hosszú-e
+                if (newPassword.length < 6) {
+                    setErrorMessage('A jelszónak legalább hat karakterből kell állnia!');
                     setSuccessMessage('');
+                } else {
+                    // Ha a bejelentkezés sikeres, elküldjük az új jelszót
+                    const updateResponse = await fetch(`${process.env.REACT_APP_API_URL}/api/profileupdate`, {
+                        method: "PUT",
+                        headers: {
+                            "Content-Type": "application/json; charset=UTF-8",
+                            "Authorization": `Bearer ${loginData.token}`
+                        },
+                        mode: "cors",
+                        body: JSON.stringify({
+                            "UserName": username,
+                            "Password": newPassword,
+                            "EmailAddress": email,
+                        })
+                    });
+                    
+                    if (updateResponse.ok) {
+                        setSuccessMessage('A jelszó sikeresen megváltoztatva!');
+                        setErrorMessage('');
+                        setTimeout(() => navigate(-1), 2000);
+                    } else {
+                        setErrorMessage('Hiba történt a jelszó megváltoztatása során.');
+                        setSuccessMessage('');
+                    }
+
                 }
             } else {
                 setErrorMessage('Hibás régi jelszó.');
@@ -75,7 +80,7 @@ const ChangePassword = ({ username, email, handleBackClick }) => {
                     <label htmlFor="old">Jelenlegi jelszó</label>
                     <div className="password-container">
                         <input
-                              type={showPassword ? 'text' : 'password'}
+                            type={showPassword ? 'text' : 'password'}
                             id='old'
                             value={oldPassword}
                             onChange={(e) => setOldPassword(e.target.value)}
@@ -88,7 +93,7 @@ const ChangePassword = ({ username, email, handleBackClick }) => {
                     <label htmlFor="new">Új jelszó</label>
                     <div className="password-container">
                         <input
-                             type={showNewPassword ? 'text' : 'password'}
+                            type={showNewPassword ? 'text' : 'password'}
                             id='new'
                             value={newPassword}
                             onChange={(e) => setNewPassword(e.target.value)}
